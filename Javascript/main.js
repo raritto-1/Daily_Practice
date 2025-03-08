46a893054b59aab533130c9b08f20f899a7e2928
Lalit <mohitmeena18803@gmail.com>
2025-03-08 14:12:54 +0530

    javascript

---
+++ Javascript/main.js
@@ -0,0 +1,13 @@
+let a = [221, 22, 45];
+// a.sort((x, y) => x - y);
+a.sort((x,y) => x - y);
+console.log(a);
+// "22" comes first (since "2" < "4")
+// "221" comes next (since "2" < "4")
+// "45" comes last
+
+
+
+let numbers = [5, 2, 8, 1];
+numbers.sort(); 
+console.log(numbers);  
